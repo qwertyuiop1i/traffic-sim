fd9df2835a9200474b1ae32cf690344d8a749f38
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-29 17:54:07 -0400

    Commit #1

---
+++ Assets/carAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carAI : MonoBehaviour
+{
+    public List<Vector2> turns= new List<Vector2>();
+
+    public Vector2 destination;
+    public Vector2 finalDestination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
